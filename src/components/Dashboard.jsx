9a418790a5f13b9a4bc0e6f8148e4f0228de6874
fabianferno <fabianferno@gmail.com>
2022-07-18 14:51:19 +0530

    Add profile

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -32,7 +32,7 @@ function Message() {
   function GithubCard(props) {
     return (
       <a
-        className="d-flex align-items-center justify-content-center"
+        className="d-flex align-items-center rounded justify-content-center"
         href={props.src}
         target="_blank"
         rel="noreferrer"
@@ -48,17 +48,17 @@ function Message() {
                 }
               : { scale: 1.08 }
           }
-          className="card bg-black rounded p-shadow text-primary m-3 d-flex align-items-center"
+          className="card bg-danger rounded p-shadow text-primary m-3 d-flex align-items-center"
           style={{
             width: "20em",
           }}
         >
-          <div className="card-body d-flex align-content-between bg-dark text-white flex-wrap">
+          <div className="card-body d-flex align-content-between bg-black text-white flex-wrap">
             <h5 className="text-white fw-bold card-title col-12 p-0">
               {props.title ? props.title : "Super Skywalker"}
             </h5>
             <img
-              src={`https://media3.giphy.com/media/QsOq3W7wCoa0sC2QEN/giphy.gif?cid=ecf05e47vw37g2a9n4oymaagriqb2ongpm9kz137qwk47b3l&rid=giphy.gif&ct=s`}
+              src="https://media1.giphy.com/media/VIWbOd3V8yVap4VQLt/giphy.gif?cid=ecf05e47el66l7t6vr83ibahm737l9kp0je8wvyrbb852kl3&rid=giphy.gif&ct=s"
               className="card-img-top"
               style={{ height: "150px", width: "100%", objectFit: "none" }}
               alt="..."
@@ -106,16 +106,47 @@ function Message() {
         transition={{ duration: 2.5 }}
       >
         <div className="text-white mt-5 text-end ">
-          <h1 className="fw-bold mb-5 text-white">Dashboard</h1>
+          {/* Profile Section */}
+
+          <div
+            style={{
+              backgroundImage:
+                "url(https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1450&h=350&q=80)",
+            }}
+            className="rounded row pt-5"
+          >
+            <div
+              style={{ background: "#000000aa" }}
+              className="mt-5 d-flex align-items-end justify-content-start"
+            >
+              <img
+                style={{
+                  width: "150px",
+                }}
+                src="https://64.media.tumblr.com/73354947f7e524a5cdadaec2ef77fc41/709a1397f7446f1d-65/s400x600/16e5a7bae6da4bb72ffe3b180de9b3b90f417bb7.png"
+                alt=""
+              />
+              <div className="ms-3 mt-2 text-white text-start">
+                <h1 className="fw-bold text-white ">Fabian Ferno</h1>
+                <p className="h5">
+                  <span className="text-warning">📍Argentina</span> | I am a
+                  software developer and I love to learn new things.
+                </p>
+              </div>
+            </div>
+          </div>
+
           <div className="mt-4 justify-content-end">
             <h3 className="fw-bold text-white">Listings</h3>
           </div>
           <h5 className="mb-3 text-secondary">
-            {" "}
             ↩️ Select a NeuralNFT🧠 to use the model
           </h5>
 
-          <motion.div className="mt-md-1 pt-md-2 pt-3 mt-3">
+          <motion.div
+            style={{ clipPath: "inset( -100vw 0 -100vw  -100vw)" }}
+            className="bg-dark mt-md-1 pt-md-2 pt-3 mt-3"
+          >
             <Marquee
               className="projects-marquee"
               direction="right"
