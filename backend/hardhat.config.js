48670da73110fcf91378edb0dde974afabb82181
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2022-07-21 21:28:51 +0530

    hardhat init

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -6,15 +6,13 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 
 const RINKEBY_RPC_URL =
     process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
 const POLYGON_MAINNET_RPC_URL =
     process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || "https://mumbai.alchemyapi.io/v2/your-api-key"
 
 // Your API key for Etherscan, obtain one at https://etherscan.io/
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
@@ -31,13 +29,16 @@ module.exports = {
             chainId: 31337,
         },
         rinkeby: {
-            url: RINKEBY_RPC_URL,
+            url: MUMBAI_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //   accounts: {
-            //     mnemonic: MNEMONIC,
-            //   },
             saveDeployments: true,
             chainId: 4,
+        },
+        mumbai: {
+            url: RINKEBY_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 80001,
         },
         polygon: {
             url: POLYGON_MAINNET_RPC_URL,
--- backend/helper-hardhat-config.js
+++ backend/helper-hardhat-config.js
@@ -1,24 +1,24 @@
 const networkConfig = {
-  default: {
-    name: "hardhat",
-  },
-  31337: {
-    name: "localhost",
-    raffleEntranceFee: "100000000000000000", // 0.1 ETH
-    callbackGasLimit: "500000", // 500,000 gas
-  },
-  80001: {
-    name: "mumbai",
-    callbackGasLimit: "500000", // 500,000 gas
-  },
-  137: {
-    name: "polygon",
-    keepersUpdateInterval: "30",
-  },
-};
+    default: {
+        name: "hardhat",
+    },
+    31337: {
+        name: "localhost",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    80001: {
+        name: "mumbai",
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    137: {
+        name: "polygon",
+        keepersUpdateInterval: "30",
+    },
+}
 
-const developmentChains = ["hardhat", "localhost"];
-const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
+const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
 // const frontEndContractsFile =
 //   "../nextjs-nft-marketplace-moralis-fcc/constants/networkMapping.json";
 // const frontEndContractsFile2 =
@@ -28,11 +28,11 @@ const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 //   "../nextjs-nft-marketplace-thegraph-fcc/constants/";
 
 module.exports = {
-  networkConfig,
-  developmentChains,
-  VERIFICATION_BLOCK_CONFIRMATIONS,
-  frontEndContractsFile,
-  frontEndContractsFile2,
-  frontEndAbiLocation,
-  frontEndAbiLocation2,
-};
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+    // frontEndContractsFile,
+    // frontEndContractsFile2,
+    // frontEndAbiLocation,
+    // frontEndAbiLocation2,
+}
